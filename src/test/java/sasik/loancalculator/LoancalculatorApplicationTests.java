a811fafa3a0fc05f5dc912399162131d26e90252
Aleksandr Spesivtsev <dart_sas@mail.ru>
2020-02-07 11:27:58 +0700

    init

---
+++ src/test/java/sasik/loancalculator/LoancalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package sasik.loancalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoancalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
